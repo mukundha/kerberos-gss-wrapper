da1272669ee005ac290e5f71e2bd1d6c14a8007c
Mukundha Madhavan <mukundha@apigee.com>
2014-08-18 21:38:35 -0700

    fixing classpath

--- kerberos.js
+++ kerberos.js
@@ -1,18 +1,14 @@
 var java = require('java');
-java.classpath.push("lib/kerberos.jar");
 
-exports.login = function (loginModule) {
-	//var strLoginModule = java.newInstanceSync("java.lang.String" , [loginModule]) ;
-	var subject = java.callStaticMethodSync( "node.security.kerberos.Kerberos" , "login" , loginModule) ;
+//FIX IT!!, 
+var path = require('fs').realpathSync('.') + '/node_modules/kerberos-gss-wrapper/lib/kerberos.jar';
+java.classpath.push(path);
 
-}
 
 exports.verifyAuthHeader = function ( loginModule , header) {
-
 	var user = java.callStaticMethodSync ("node.security.kerberos.Kerberos" , "verifyAuthHeader" , loginModule , header);
 	return user ;
 }
-
 
 exports.delegateCreds = function (loginModule, header, serverName) {
 	var token = java.callStaticMethodSync ("node.security.kerberos.Kerberos" , "getDelegateCreds" , loginModule , header , serverName);
